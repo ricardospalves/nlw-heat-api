d68f166313dee8c988c1cd82b45d792b6e0a15cd
ricardospalves <ricardospalves@gmail.com>
2022-02-06 02:40:47 -0300

    Adicionar a rota para o perfil do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
 import { GetLatestMessagesController } from './controllers/GetLatestMessagesController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
@@ -15,5 +16,7 @@ router.post(
 )
 
 router.get('/messages/latest', new GetLatestMessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
